de55ca4221682a61fcc64985b63326a3da4522a3
SML <ronnymayamba94@gmail.com>
2024-02-23 13:52:34 +0100

    Font

--- src/app/components/layouts/header.tsx
+++ src/app/components/layouts/header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
             <Link href={"/"} className='text-[25px] text-white font-[600] cursor-default'>SML PORTFOLIO</Link>
             <IoMdMenu className=" text-[3.2rem] text-white hidden max-lg:block" />
             <nav className='inline-block max-lg:hidden'>
-                <Link href={"/#accueil"} className='ml-[3.5rem] text-white no-underline text-3xl	font-[500] transition duration-[.3s] hover:text-[#8a2be2]'>accueil</Link>
-                <Link href={"/#about"} className='ml-[3.5rem] text-white no-underline text-3xl	font-[500] transition duration-[.3s] hover:text-[#8a2be2]'>A propos</Link>
-                <Link href={"/#skill"} className='ml-35px no-underline text-3xl	font-[500] transition duration-[.3s] hover:text-[#8a2be2]'>Compétence</Link>
-                <Link href={"/#portfolio"} className='ml-35px no-underline text-3xl	font-[500] transition duration-[.3s] hover:text-[#8a2be2]'>Portfolio</Link>
-                <Link href={"/#contact"} className='ml-35px no-underline text-3xl	font-[500] transition duration-[.3s] hover:text-[#8a2be2]'>Contact</Link>
+                <Link href={"/#accueil"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>accueil</Link>
+                <Link href={"/#about"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>A propos</Link>
+                <Link href={"/#skill"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Compétence</Link>
+                <Link href={"/#portfolio"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Portfolio</Link>
+                <Link href={"/#contact"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Contact</Link>
             </nav>
         </header>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
+import { Poppins } from "@next/font/google";
 import type { Metadata } from "next";
-import "./globals.css";
-import Header from "./components/layouts/header";
 import Footer from "./components/layouts/footer";
+import Header from "./components/layouts/header";
+import "./globals.css";
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900']
+})
 
 export const metadata: Metadata = {
   title: "SML",
@@ -14,12 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
-      <body>
-        <Header/>
+    <html lang="fr" >
+      <body className={poppins.className}>
+        <Header />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
