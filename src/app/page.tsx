d571be7f4a2dc2d0bef282c9793e240584133742
SML <ronnymayamba94@gmail.com>
2024-02-24 13:09:37 +0100

    punycode integrer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Accueil from '@/app/components/contents/home';
 
 export default function Pages() {
-
+  const punycode = require('punycode');
   return (
     <main>
       <div id="accueil">
