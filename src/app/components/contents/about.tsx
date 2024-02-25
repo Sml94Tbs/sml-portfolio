a446e569a65f9d423a7a95ae6121600379fdd551
SML <ronnymayamba94@gmail.com>
2024-02-25 15:46:32 +0100

    About fix

--- src/app/components/contents/about.tsx
+++ src/app/components/contents/about.tsx
@@ -1,6 +1,12 @@
 const About = () => {
-    return(
-        <section className=" flex justify-center items-center gap-[2rem] bg-transparent backdrop-blur-[15px]"></section>
+    return (
+        <section className=" flex justify-center items-center gap-[2rem] bg-transparent backdrop-blur-[15px]">
+            <div className=" justify-between ml-[1.6rem]">
+                <h2 className=" text-left text-[4.5rem] font-[700] text-white leading-1.2">A propos de Moi</h2>
+                <h3 className=" text-[2.6rem] text-white font-[700]">Samuel Mayamba</h3>
+                <p className=" text-white text-[1.6rem] m-[2rem_0_3rem]">Je suis âgé de 19 ans, actuellement étudiant en 2e année de BTS SIO option SLAM au Lycée polyvalent de Cachan. </p>
+            </div>
+        </section>
     )
 }
 
