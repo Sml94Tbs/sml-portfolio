df8177eb4e27d9745f1ebe656de2a3a203c42e15
SML <ronnymayamba94@gmail.com>
2024-02-25 14:45:49 +0100

    Header finish

--- src/app/components/layouts/header.tsx
+++ src/app/components/layouts/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         setIsClick(!isClick);
     }
     return (
-        <header className='top-0 left-0 w-full p-[20px_120px] fixed bg-transparent flex items-center justify-between z-100 backdrop-blur-[16px] border-solid border-b-[1px] border-b-[#ffffff5b] max-lg:p-[2rem_3%]'>
+        <header className='top-0 left-0 w-full p-[20px_120px] fixed bg-transparent flex items-center justify-between z-[101] backdrop-blur-[16px] border-solid border-b-[1px] border-b-[#ffffff5b] max-lg:p-[2rem_3%]'>
             <Link href={"/"} className='text-[25px] text-white font-[600] cursor-default'>SML PORTFOLIO</Link>
             <button className=" hidden max-lg:block text-[3.2rem] text-white" onClick={toogleNavbar}>
                 {isClick ? (
@@ -20,7 +20,7 @@ const Header = () => {
                 )}
             </button>
             {isClick && (
-                <nav className=' hidden max-lg:block absolute top-full left-0 w-full p-[1rem_3%] bg-transparent backdrop-blur-[16px] border-t-[.1rem] border-t-solid border-t-[#0000001a]'>
+                <nav className=' hidden max-lg:block absolute top-full left-0 w-full p-[1rem_3%] bg-[#462956d4] backdrop-blur-[16px] border-t-[.1rem] border-t-solid border-t-[#0000001a]'>
                     <Link href={"/#accueil"} className='m-[3rem_0] block text-white no-underline text-[2rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Accueil</Link>
                     <Link href={"/#about"} className='m-[3rem_0] block text-white no-underline text-[2rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>A propos</Link>
                     <Link href={"/#skill"} className='m-[3rem_0] block text-white no-underline text-[2rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Compétence</Link>
