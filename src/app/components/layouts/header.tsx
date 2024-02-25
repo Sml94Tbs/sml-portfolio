e0f08330c292848ebb92cf4657c8bf09d71eab87
SML <ronnymayamba94@gmail.com>
2024-02-25 13:41:31 +0100

    Css fix

--- src/app/components/layouts/header.tsx
+++ src/app/components/layouts/header.tsx
@@ -1,24 +1,17 @@
 'use client'
 import Link from "next/link";
 import { useState } from "react";
-import { IoIosClose, IoMdMenu } from "react-icons/io";
+import { IoIosClose, IoIosMenu } from "react-icons/io";
 
 const Header = () => {
     const [isClick, setIsClick] = useState(false);
-    const handleClick = () => {
+    const toogleNavbar = () => {
         console.log('Ca marche')
         setIsClick(!isClick);
     }
     return (
         <header className='top-0 left-0 w-full p-[20px_120px] fixed bg-transparent flex items-center justify-between z-100 backdrop-blur-[16px] border-solid border-b-[1px] border-b-[#ffffff5b] max-lg:p-[1rem_3%]'>
             <Link href={"/"} className='text-[25px] text-white font-[600] cursor-default'>SML PORTFOLIO</Link>
-            <button className="text-[3.2rem] text-white hidden max-lg:block cursor-pointer" onClick={handleClick} >
-                {isClick ? (
-                    <IoIosClose />
-                ) : (
-                    <IoMdMenu />
-                )}
-            </button>
             <nav className='inline-block max-lg:hidden max-lg:top-full max-lg:absolute max-lg:p-[1rem_3%] max-lg:border-t-[.1rem]'>
                 <Link href={"/#accueil"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Accueil</Link>
                 <Link href={"/#about"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>A propos</Link>
@@ -26,15 +19,15 @@ const Header = () => {
                 <Link href={"/#portfolio"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Portfolio</Link>
                 <Link href={"/#contact"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Contact</Link>
             </nav>
-            {isClick && (
-                <nav className='inline-block max-lg:top-full max-lg:absolute max-lg:p-[1rem_3%] max-lg:border-t-[.1rem]'>
-                    <Link href={"/#accueil"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Accueil</Link>
-                    <Link href={"/#about"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>A propos</Link>
-                    <Link href={"/#skill"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Compétence</Link>
-                    <Link href={"/#portfolio"} className='ml-[3.5rem] text-white no-underline text-[1.8rem]	font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Portfolio</Link>
-                    <Link href={"/#contact"} className='ml-[3.5rem] text-white no-underline text-[1.8rem] font-[500] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.3s] hover:text-[#8a2be2]'>Contact</Link>
-                </nav>
-            )}
+            <div className=" hidden max-lg:block">
+                <button className=" text-[3.2rem] text-white" onClick={toogleNavbar}>
+                    {isClick ? (
+                        <IoIosClose />
+                    ) : (
+                        <IoIosMenu />
+                    )}
+                </button>
+            </div>
         </header>
     );
 }
