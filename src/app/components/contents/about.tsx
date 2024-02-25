8d51c9fad704ef714fb9fba2e3b87130b3bce084
SML <ronnymayamba94@gmail.com>
2024-02-25 15:19:31 +0100

    About init

---
+++ src/app/components/contents/about.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return(
+        <section className=" flex justify-center items-center gap-[2rem] bg-transparent backdrop-blur-[15px]"></section>
+    )
+}
+
+export default About
