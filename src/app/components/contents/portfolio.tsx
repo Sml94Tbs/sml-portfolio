11ab93014e94ab32fdd6a1e8decef872655eec04
SML <ronnymayamba94@gmail.com>
2024-03-13 10:12:31 +0100

    Fix

--- src/app/components/contents/portfolio.tsx
+++ src/app/components/contents/portfolio.tsx
@@ -6,7 +6,12 @@ const Portfolio = () => {
             <h2 className=" text-center text-[4.5rem] font-[700] text-white mb-[4rem]">Mon Portfolio</h2>
             <h4 className=" text-[2.4rem] text-center font-[700] text-white mb-[2rem]">Sur cette section vous pourrez retrouver mes travaux</h4>
             <div className=" grid grid-cols-[repeat(3,_1fr)]">
-                <PortfolioBox/>
+                <PortfolioBox
+                titre={"portfolio"}
+                image={"#"}
+                height={1800}
+                width={1200}
+                />
             </div>
         </section>
     );
--- src/app/components/portfolioBox.tsx
+++ src/app/components/portfolioBox.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 
-const PortfolioBox = () => {
+const PortfolioBox = ({ titre, image, height , width}: { titre: string, image: string, height: number, width : number }) => {
     return (
         <div className=" relative rounded-[2rem]  shadow-[0_0_1rem_#4d4d4d] overflow-hidden flex group">
-            
+            <Image src={image} width={width} height={height} alt="Portfolio-image" />
         </div>
     )
 }
