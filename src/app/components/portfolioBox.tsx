c1ae7600583b60d7948a7e5d47aa6577e9b8545b
SML <ronnymayamba94@gmail.com>
2024-03-13 09:25:01 +0100

    fix

---
+++ src/app/components/portfolioBox.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+const PortfolioBox = () => {
+    return (
+        <div className=" relative rounded-[2rem]  shadow-[0_0_1rem_#4d4d4d] overflow-hidden flex group">
+            
+        </div>
+    )
+}
+export default PortfolioBox
