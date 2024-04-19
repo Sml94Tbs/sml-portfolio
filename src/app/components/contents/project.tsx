893a25073d37455e31696448736ad4950460732d
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-19 17:18:09 +0200

    init project

---
+++ src/app/components/contents/project.tsx
@@ -0,0 +1,10 @@
+const Project = () => {
+    return(
+        <section>
+            <h1 className=" text-center text-[4.5rem] font-[700] text-white">Mes projets</h1>
+        </section>
+    );
+}
+
+export default Project;
+
