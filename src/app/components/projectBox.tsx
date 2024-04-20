15731e5b336dea4f73c3fb33838799b9a7288c60
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-20 13:10:59 +0200

    add

--- src/app/components/projectBox.tsx
+++ src/app/components/projectBox.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
+import Link from "next/link";
 
-const ProjectBox = ({icon, height, width, image}:{icon : any,height: number, width: number, image : string}) => {
+const ProjectBox = ({project,icon, height, width, image, link}:{project: string,icon : any,height: number, width: number, image : string, link: any}) => {
     return(
-        <div className=" flex flex-col   bg-[#5f3869] overflow-hidden rounded-[2rem] border border-solid border-[#da7bff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s] hover:border-[#ebb7ff] hover:scale-[1.02]">
-            <Image src={image} alt=" Project Image" width={width} height={height}/>
+        <Link href={link}>
+        <div className=" flex flex-col bg-[#5f3869] overflow-hidden rounded-[2rem] border border-solid border-[#da7bff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s] hover:border-[#ebb7ff] hover:scale-[1.02]">
+            <Image src={image} alt=" Project Image" width={width} height={height} className=" w-full"/>
             <div className="p-[2rem]">
-                <h4 className=" text-[2.5rem]">Site Bonbon</h4>
-                <i>{icon}</i>
+                <h4 className=" text-[2.5rem] font-[600] text-[#fdbaff]">{project}</h4>
+                <i className=" flex text-[3rem] text-[#fdbaff] gap-[1.6rem]">{icon}</i>
             </div>
         </div>
+        </Link>
     )
 }
 
