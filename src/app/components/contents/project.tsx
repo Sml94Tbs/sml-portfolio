6ad83dc1f31dcd4d24c3b914d565fccfba515024
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-22 20:16:15 +0200

    add new project

--- src/app/components/contents/project.tsx
+++ src/app/components/contents/project.tsx
@@ -1,7 +1,8 @@
 import ProjectBox from "@/app/components/projectBox";
 import Link from "next/link";
-import { FaHtml5, FaLaravel, FaLongArrowAltLeft, FaPhp,FaCss3 } from "react-icons/fa";
+import { FaHtml5, FaLaravel, FaLongArrowAltLeft, FaPhp,FaCss3, FaBootstrap, FaAndroid } from "react-icons/fa";
 import { SiCsharp } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
 
 const Project = () => {
     return (
@@ -10,12 +11,36 @@ const Project = () => {
             <h5 className=" text-white text-center text-[1.6rem] mb-[3rem]">Vous pouvez retrouver tout mes project que j&apos;ai pu réaliser</h5>
             <div className=" grid grid-cols-[repeat(3,_1fr)] items-center gap-[2.5rem] max-lg:grid-cols-[repeat(2,_1fr)] max-sm:grid-cols-[1fr]">
                 <ProjectBox
+                    project={"Boostrap Website"}
+                    image={"/images/Capture d'écran 2024-04-22 193402.png"}
+                    icon={<><FaHtml5 /><FaCss3 /> <FaBootstrap /></>}
+                    height={1919}
+                    width={1079}
+                    link={"https://github.com/Sml94Tbs/Bootstrap-Website"}
+                />
+                <ProjectBox
                     project={"Site Bonbon"}
                     image={"/images/Capture d'écran 2024-04-19 182620.png"}
                     icon={<><FaHtml5 /><FaCss3 /> <FaPhp /></>}
                     height={1919}
                     width={1079}
                     link={"#"}
+                />
+                <ProjectBox
+                    project={"Portfolio V1 HTML Static"}
+                    image={"/images/Capture d'écran 2024-04-22 193914.png"}
+                    icon={<><FaHtml5 /><FaCss3 /> <IoLogoJavascript /></>}
+                    height={1919}
+                    width={1079}
+                    link={"#"}
+                />
+                <ProjectBox
+                    project={"Projet Gestion École"}
+                    image={"/images/Capture d'écran 2023-11-20 222514.png"}
+                    icon={<><FaHtml5 /><FaCss3 /> <FaPhp />  <FaLaravel /></>}
+                    height={1919}
+                    width={1079}
+                    link={"https://github.com/Sml94Tbs/Laravel-Project---Gestion-Ecole"}
                 />
                 <ProjectBox
                     project={"Projet SicilyLines"}
@@ -34,12 +59,12 @@ const Project = () => {
                     link={"https://github.com/Sml94Tbs/sicily"}
                 />
                 <ProjectBox
-                    project={"Projet Gestion École"}
-                    image={"/images/Capture d'écran 2023-11-20 222514.png"}
-                    icon={<><FaHtml5 /><FaCss3 /> <FaPhp />  <FaLaravel /></>}
+                    project={"Projet Android Login"}
+                    image={"/images/android-digital-art-8k-wallpaper-uhdpaper.com-910@0@f.jpg"}
+                    icon={<><FaAndroid /></>}
                     height={1919}
                     width={1079}
-                    link={"https://github.com/Sml94Tbs/Laravel-Project---Gestion-Ecole"}
+                    link={"https://github.com/Sml94Tbs/sicily"}
                 />
             </div>
             <div className=" flex items-center justify-center mt-[2rem]">
