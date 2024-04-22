7778a3177a2575bac46f4ac0ab578d4028e6a75c
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-22 19:12:11 +0200

    add parcours s fix

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -1,6 +1,11 @@
-const Timeline = () =>{
+const Timeline = ({titre, date, lieu}:{titre : string, date : string, lieu : string}) =>{
     return (
-        <div>Bonjour</div>
+        <div className=" before:top-0 mb-[3.2rem] rounded-[2rem] before:content-[''] before:absolute before:left-[-1.05rem] before:w-[20px] before:h-[20px] before:bg-[#fff] before:rounded-[50%] p-[2.4rem] border-solid border-[0.2rem]">
+            <h3 className="text-[1.872rem] font-[600] text-white">{titre}</h3>
+            <small className=" text-[1.1rem] text-white">{date}</small>
+            <p className=" text-[1.6rem] text-white">{lieu}</p>
+            <br />
+        </div>
     )
 }
 
