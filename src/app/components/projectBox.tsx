18d227798a66d87935a799b408d96990dbc5748e
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-24 22:33:27 +0200

    add target

--- src/app/components/projectBox.tsx
+++ src/app/components/projectBox.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ProjectBox = ({project,icon, height, width, image, link}:{project: string,icon : any,height: number, width: number, image : string, link: any}) => {
     return(
-        <Link href={link}>
+        <Link target="_blank" href={link}>
         <div className=" flex flex-col bg-[#5f3869] overflow-hidden rounded-[2rem] border border-solid border-[#da7bff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s] hover:border-[#ebb7ff] hover:scale-[1.02]">
             <Image src={image} alt=" Project Image" width={width} height={height} className=" w-full"/>
             <div className="p-[2rem]">
