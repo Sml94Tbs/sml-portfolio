adfccb6e6b0c62025bfb5348a8643f8455b2a331
Sml94Tbs <136707863+Sml94Tbs@users.noreply.github.com>
2024-04-25 09:47:29 +0200

    Update about.tsx

--- src/app/components/contents/about.tsx
+++ src/app/components/contents/about.tsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className=" justify-between ml-[1.6rem]">
                 <h2 className=" text-left text-[4.5rem] font-[700] text-white leading-1.2">A propos de Moi</h2>
                 <h3 className=" text-[2.6rem] text-white font-[700]">Samuel Mayamba</h3>
-                <p className=" text-white text-[1.6rem] m-[2rem_0_3rem]">Je suis âgé de 19 ans, actuellement étudiant en 2e année de BTS SIO option SLAM au Lycée polyvalent de Cachan. </p>
+                <p className=" text-white text-[1.6rem] m-[2rem_0_3rem]">Je suis âgé de 20 ans, actuellement étudiant en 2e année de BTS SIO option SLAM au Lycée polyvalent de Cachan. </p>
             </div>
             <div className=" flex justify-center items-center">
                 <div className=" flex gap-x-[8rem] max-md:flex-col">
